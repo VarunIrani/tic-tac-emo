6f1715f758b37597604c2f758e8e0ee57497cec7
VarunIrani <varunirani0505@gmail.com>
2021-07-05 20:04:38 +0530

    Checked winner and showing dialog

--- lib/main.dart
+++ lib/main.dart
@@ -80,11 +80,15 @@ class _TicTacEmoState extends State<TicTacEmo> {
 
   selectField(String value, int x, int y) {
     if (value == Players.none) {
-      final newValue = lastMove == Players.X ? Players.O: Players.X;
+      final newValue = lastMove == Players.X ? Players.O : Players.X;
       setState(() {
         lastMove = newValue!;
         matrix[x][y] = newValue;
       });
+    }
+
+    if (isWinner(x, y)) {
+      showEndDialog("Player $lastMove Won!");
     }
   }
 
@@ -101,5 +105,54 @@ class _TicTacEmoState extends State<TicTacEmo> {
   getBackgroundColor() {
     final thisMove = lastMove == Players.X ? Players.O : Players.X;
     return getFieldColor(thisMove!).withAlpha(150);
+  }
+
+  Future showEndDialog(String title) async {
+    return showDialog(
+      context: context,
+      barrierDismissible: false,
+      builder: (context) => AlertDialog(
+        title: Text(title),
+        content: Text("Would you like to play again or reset the score?"),
+        actions: [
+          TextButton(
+            onPressed: () {
+              setEmptyFields();
+              Navigator.of(context).pop();
+            },
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text("Play Again"),
+            ),
+          ),
+          TextButton(
+            onPressed: () {
+              // TODO: Reset Score
+              Navigator.of(context).pop();
+            },
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text("Reset Score"),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  bool isWinner(int x, int y) {
+    int row = 0, col = 0, diag = 0, rdiag = 0;
+
+    final player = matrix[x][y];
+    final n = countMatrix;
+
+    for (int i = 0; i < n; i++) {
+      if (matrix[x][i] == player) row++;
+      if (matrix[i][y] == player) col++;
+      if (matrix[i][i] == player) diag++;
+      if (matrix[i][n - i - 1] == player) rdiag++;
+    }
+
+    return row == n || col == n || diag == n || rdiag == n;
   }
 }
