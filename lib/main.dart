92040e3d98ccc4600f6878b9e63727ab65cb0123
VarunIrani <varunirani0505@gmail.com>
2021-07-05 19:40:53 +0530

    Building empty fields

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:tic_tac_emo/players.dart';
+import 'package:tic_tac_emo/utils.dart';
 
 Future main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -7,12 +9,72 @@ Future main() async {
     DeviceOrientation.portraitUp,
     DeviceOrientation.portraitDown,
   ]);
-  runApp(TicTacEmo());
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: TicTacEmo(),
+    ),
+  );
 }
 
-class TicTacEmo extends StatelessWidget {
+class TicTacEmo extends StatefulWidget {
+  @override
+  _TicTacEmoState createState() => _TicTacEmoState();
+}
+
+class _TicTacEmoState extends State<TicTacEmo> {
+  final int countMatrix = 3;
+  final double size = 92;
+
+  late List<List<String>> matrix;
+
+  @override
+  void initState() {
+    super.initState();
+    setEmptyFields();
+  }
+
+  void setEmptyFields() => setState(() => matrix = List.generate(
+      countMatrix, (_) => List.generate(countMatrix, (_) => Players.none)));
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      backgroundColor: Colors.blue,
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: Utils.modelBuilder(matrix, (x, value) => buildRow(x)),
+      ),
+    );
+  }
+
+  buildRow(int x) {
+    final values = matrix[x];
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: Utils.modelBuilder(values, (y, value) => buildField(x, y)),
+    );
+  }
+
+  buildField(int x, int y) {
+    // Player
+    final value = matrix[x][y];
+
+    return Container(
+      margin: EdgeInsets.all(4),
+      child: ElevatedButton(
+        onPressed: () {
+          print("$x, $y");
+        },
+        style: ElevatedButton.styleFrom(
+          minimumSize: Size(size, size),
+          primary: Colors.white,
+        ),
+        child: Text(
+          value,
+          style: TextStyle(fontSize: 32),
+        ),
+      ),
+    );
   }
 }
