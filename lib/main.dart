e8525066355f6b509cec8310b97c06bced8a5e5d
VarunIrani <varunirani0505@gmail.com>
2021-07-05 19:10:34 +0530

    Starter code

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:theme_provider/theme_provider.dart';
-import 'package:tic_tac_emo/screens/game.dart';
-import 'package:tic_tac_emo/screens/menu.dart';
-import 'package:tic_tac_emo/themes.dart';
 
 Future main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -11,32 +7,12 @@ Future main() async {
     DeviceOrientation.portraitUp,
     DeviceOrientation.portraitDown,
   ]);
-  runApp(MyApp());
+  runApp(TicTacEmo());
 }
 
-class MyApp extends StatelessWidget {
+class TicTacEmo extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return ThemeProvider(
-      themes: [
-        MyAppThemes.lightTheme,
-        MyAppThemes.darkTheme,
-      ],
-      defaultThemeId: MyAppThemes.darkThemeID,
-      child: ThemeConsumer(
-        child: Builder(
-          builder: (themeContext) => MaterialApp(
-            title: 'Tic Tac Toe',
-            debugShowCheckedModeBanner: false,
-            initialRoute: '/',
-            theme: ThemeProvider.themeOf(themeContext).data,
-            routes: {
-              '/': (context) => MenuScreen(),
-              '/game': (context) => GameScreen(),
-            },
-          ),
-        ),
-      ),
-    );
+    return Container();
   }
 }
